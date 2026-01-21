733226c3ee257cec7a4c60e096bb018dbd154037
kefengyoyo <56055045+kefengyoyo@users.noreply.github.com>
2026-01-22 07:35:00 +0800

    更新 CamScanner2026.js

--- CamScanner2026.js
+++ CamScanner2026.js
@@ -28,7 +28,7 @@ https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url simple-r
 [MITM]
 hostname = ap*.intsig.net
 
-**************************
+**************************/
 let obj = JSON.parse($response.body);
 obj = {"data":{"psnl_vip_property":{"expiry":4102415999,"svip": 1,"nxt_renew_tm":4102415999}}};
 $done({body: JSON.stringify(obj)});
