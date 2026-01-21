67bf0baeb1edb009ee98e7bdb8080137b1002ad5
kefengyoyo <56055045+kefengyoyo@users.noreply.github.com>
2026-01-22 07:15:53 +0800

    更新 CamScanner2026.js

--- CamScanner2026.js
+++ CamScanner2026.js
@@ -30,5 +30,5 @@ hostname = ap*.intsig.net
 
 **************************/
 let obj = JSON.parse($response.body);
-obj = {"data":{"psnl_vip_property":{"expiry":"2013017600"}}};
+obj = {"data":{"psnl_vip_property":{"expiry":4102415999,"svip": 1,"nxt_renew_tm":4102415999}}};
 $done({body: JSON.stringify(obj)});
